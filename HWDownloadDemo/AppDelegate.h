79a1a38a5c5ec471961b961d47f1aef5c77e75d3
wangqibin <wangqibin@wangqibin.local>
2018-05-21 16:40:29 +0800

    Initial Commit

---
+++ HWDownloadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HWDownloadDemo
+//
+//  Created by wangqibin on 2018/5/21.
+//  Copyright © 2018年 hero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HWDownloadDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HWDownloadDemo
+//
+//  Created by wangqibin on 2018/5/21.
+//  Copyright © 2018年 hero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
